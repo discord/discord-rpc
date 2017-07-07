7c92afb39240d493feebbd2701cdb80e29cd9e12
Chris Marsh <chris@discordapp.com>
2017-07-07 14:00:05 -0700

    clean that out

--- include/discord-rpc.h
+++ include/discord-rpc.h
@@ -38,32 +38,6 @@ void Discord_UpdatePresence(const DiscordRichPresence* presence);
 /* checks for incoming messages, dispatches callbacks */
 void Discord_Update();
 
-/* later
-
-struct DiscordChannelEventHandlers {
-    void (*messageCreate)(const DiscordMessage* message);
-    void (*messageUpdate)(const DiscordMessage* message);
-    void (*messageDelete)(const DiscordMessage* message);
-    void (*voiceStateCreate)(const DiscordVoiceState* state);
-    void (*voiceStateDelete)(const DiscordVoiceState* state);
-    void (*speakingStart)(const DiscordSpeakingState* state);
-    void (*speakingStop)(const DiscordSpeakingState* state);    
-};
-// Call this to subscribe to events in a specific voice or text channel
-void Discord_Subscribe(const char* channelId, const DiscordChannelEventHandlers* handlers);
-
-// TBD RPC Requests
-void Discord_Authenticate();
-void Discord_Authorize();
-void Discord_GetGuilds();
-void Discord_GetChannels();
-void Discord_GetChannel();
-void Discord_SelectVoiceChannel();
-void Discord_SelectTextChannel();
-void Discord_SendMessage();
-
-*/
-
 #ifdef  __cplusplus
 } /* extern "C" */
 #endif
