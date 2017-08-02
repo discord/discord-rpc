7fe7e2ab5334a4e7502a608d541163a8fd4fe870
Chris Marsh <chris@discordapp.com>
2017-08-02 11:31:35 -0700

    Initial pass at a unity example... no callbacks yet.

---
+++ examples/button-clicker/Assets/DiscordController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscordController : MonoBehaviour {
+    public DiscordRpc.EventHandlers handlers;
+    public DiscordRpc.RichPresence presence;
+    public string applicationId;
+    public int callbackCalls;
+    public int clickCounter;
+
+    public void OnClick()
+    {
+        Debug.Log("Discord: on click!");
+        clickCounter++;
+
+        presence.details = string.Format("Button clicked {0} times", clickCounter);
+
+        DiscordRpc.UpdatePresence(ref presence);
+    }
+
+    public void ReadyCallback()
+    {
+        ++callbackCalls;
+        Debug.Log("Discord: ready");
+    }
+
+    public void DisconnectedCallback(int errorCode, string message)
+    {
+        ++callbackCalls;
+        Debug.Log(string.Format("Discord: disconnect {0}: {1}", errorCode, message));
+    }
+
+    public void ErrorCallback(int errorCode, string message)
+    {
+        ++callbackCalls;
+        Debug.Log(string.Format("Discord: error {0}: {1}", errorCode, message));
+    }
+
+    public void JoinCallback(string secret)
+    {
+        ++callbackCalls;
+        Debug.Log(string.Format("Discord: join ({0})", secret));
+    }
+
+    public void SpectateCallback(string secret)
+    {
+        ++callbackCalls;
+        Debug.Log(string.Format("Discord: spectate ({0})", secret));
+    }
+
+    void Start () {
+        handlers = new DiscordRpc.EventHandlers();
+        handlers.readyCallback = ReadyCallback;
+        handlers.disconnectedCallback = DisconnectedCallback;
+        handlers.errorCallback = ErrorCallback;
+        handlers.joinCallback = JoinCallback;
+        handlers.spectateCallback = SpectateCallback;
+    }
+	
+	void Update () {
+
+    }
+
+    void OnEnable()
+    {
+        var p = System.Diagnostics.Process.GetCurrentProcess();
+        Debug.Log(string.Format("Discord: init {0}", p.Id));
+        callbackCalls = 0;
+        DiscordRpc.Initialize(applicationId, ref handlers, true);
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Discord: shutdown");
+        DiscordRpc.Shutdown();
+    }
+
+    void OnDestroy()
+    {
+        
+    }
+}
---
+++ examples/button-clicker/Assets/DiscordRpc.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.InteropServices;
+
+public class DiscordRpc
+{
+    [UnmanagedFunctionPointerAttribute(CallingConvention.Cdecl)]
+    public delegate void ReadyCallback();
+
+    [UnmanagedFunctionPointerAttribute(CallingConvention.Cdecl)]
+    public delegate void DisconnectedCallback(int errorCode, string message);
+
+    [UnmanagedFunctionPointerAttribute(CallingConvention.Cdecl)]
+    public delegate void ErrorCallback(int errorCode, string message);
+
+    [UnmanagedFunctionPointerAttribute(CallingConvention.Cdecl)]
+    public delegate void JoinCallback(string secret);
+
+    [UnmanagedFunctionPointerAttribute(CallingConvention.Cdecl)]
+    public delegate void SpectateCallback(string secret);
+
+    [System.Serializable]
+    public struct EventHandlers
+    {
+        public ReadyCallback readyCallback;
+        public DisconnectedCallback disconnectedCallback;
+        public ErrorCallback errorCallback;
+        public JoinCallback joinCallback;
+        public SpectateCallback spectateCallback;
+    }
+
+    [System.Serializable]
+    public struct RichPresence
+    {
+        public string state; /* max 128 bytes */
+        public string details; /* max 128 bytes */
+        public long startTimestamp;
+        public long endTimestamp;
+        public string largeImageKey; /* max 32 bytes */
+        public string largeImageText; /* max 128 bytes */
+        public string smallImageKey; /* max 32 bytes */
+        public string smallImageText; /* max 128 bytes */
+        public string partyId; /* max 128 bytes */
+        public int partySize;
+        public int partyMax;
+        public string matchSecret; /* max 128 bytes */
+        public string joinSecret; /* max 128 bytes */
+        public string spectateSecret; /* max 128 bytes */
+        public bool instance;
+    }
+
+    [DllImport("discord-rpc", EntryPoint = "Discord_Initialize")]
+    public static extern void Initialize(string applicationId, ref EventHandlers handlers, bool autoRegister);
+
+    [DllImport("discord-rpc", EntryPoint = "Discord_Shutdown")]
+    public static extern void Shutdown();
+
+    [DllImport("discord-rpc", EntryPoint = "Discord_RunCallbacks")]
+    public static extern void RunCallbacks();
+
+    [DllImport("discord-rpc", EntryPoint = "Discord_UpdatePresence")]
+    public static extern void UpdatePresence(ref RichPresence presence);
+}
+
