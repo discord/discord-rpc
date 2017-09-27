ae34d0d3605dcd17c107a79b79119a71a3d684ba
SK83RJOSH <sk83rjosh83@yahoo.com>
2017-09-27 14:47:52 -0400

    Use simplified attribute names and ensure function calls are made with CallingConvention.Cdecl

--- examples/button-clicker/Assets/DiscordRpc.cs
+++ examples/button-clicker/Assets/DiscordRpc.cs
@@ -2,19 +2,19 @@
 
 public class DiscordRpc
 {
-    [UnmanagedFunctionPointerAttribute(CallingConvention.Cdecl)]
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void ReadyCallback();
 
-    [UnmanagedFunctionPointerAttribute(CallingConvention.Cdecl)]
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void DisconnectedCallback(int errorCode, string message);
 
-    [UnmanagedFunctionPointerAttribute(CallingConvention.Cdecl)]
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void ErrorCallback(int errorCode, string message);
 
-    [UnmanagedFunctionPointerAttribute(CallingConvention.Cdecl)]
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void JoinCallback(string secret);
 
-    [UnmanagedFunctionPointerAttribute(CallingConvention.Cdecl)]
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void SpectateCallback(string secret);
 
     public struct EventHandlers
@@ -46,16 +46,16 @@ public class DiscordRpc
         public bool instance;
     }
 
-    [DllImport("discord-rpc", EntryPoint = "Discord_Initialize")]
+    [DllImport("discord-rpc", EntryPoint = "Discord_Initialize", CallingConvention = CallingConvention.Cdecl)]
     public static extern void Initialize(string applicationId, ref EventHandlers handlers, bool autoRegister, string optionalSteamId);
 
-    [DllImport("discord-rpc", EntryPoint = "Discord_Shutdown")]
+    [DllImport("discord-rpc", EntryPoint = "Discord_Shutdown", CallingConvention = CallingConvention.Cdecl)]
     public static extern void Shutdown();
 
-    [DllImport("discord-rpc", EntryPoint = "Discord_RunCallbacks")]
+    [DllImport("discord-rpc", EntryPoint = "Discord_RunCallbacks", CallingConvention = CallingConvention.Cdecl)]
     public static extern void RunCallbacks();
 
-    [DllImport("discord-rpc", EntryPoint = "Discord_UpdatePresence")]
+    [DllImport("discord-rpc", EntryPoint = "Discord_UpdatePresence", CallingConvention = CallingConvention.Cdecl)]
     public static extern void UpdatePresence(ref RichPresence presence);
 }
 
