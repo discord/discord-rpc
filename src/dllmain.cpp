7185eaedc2ca4011877f752fbe38ab9a92fffc4a
Michał Janiszewski <janisozaur@gmail.com>
2018-02-14 23:02:10 +0100

    Ignore missing declarations around DllMain

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,6 +1,15 @@
 #include <windows.h>
 
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#endif
+
 BOOL WINAPI DllMain(HMODULE, DWORD, LPVOID)
 {
     return TRUE;
 }
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
