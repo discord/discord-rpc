578eb6de7cf8295e63253ccec3e9363a235e2b1d
Michał Janiszewski <janisozaur@users.noreply.github.com>
2018-02-15 23:36:31 +0100

    Provide fake DllMain declaration to fix missing-declarations warning (#130)

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,5 +1,7 @@
 #include <windows.h>
 
+// outsmart GCC's missing-declarations warning
+BOOL WINAPI DllMain(HMODULE, DWORD, LPVOID);
 BOOL WINAPI DllMain(HMODULE, DWORD, LPVOID)
 {
     return TRUE;
