2ce9fe068bfb51a223465f57c29d15bf5f4f6336
Codecat <spansjh@gmail.com>
2018-04-04 19:00:24 +0200

    Syntax change to avoid gcc 4.8 segfaulting (#162)

--- src/msg_queue.h
+++ src/msg_queue.h
@@ -7,7 +7,7 @@
 
 template <typename ElementType, size_t QueueSize>
 class MsgQueue {
-    ElementType queue_[QueueSize]{};
+    ElementType queue_[QueueSize];
     std::atomic_uint nextAdd_{0};
     std::atomic_uint nextSend_{0};
     std::atomic_uint pendingSends_{0};
