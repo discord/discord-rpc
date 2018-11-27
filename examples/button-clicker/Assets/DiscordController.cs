e6390c8c4190b59f1cc98bcec688e0037b84974d
Mason Sciotti <mason.sciotti@gmail.com>
2018-11-27 09:19:14 -0800

    Unity il2cpp support (#249)
    * Initial il2cpp support attempts
    
    * Fix crashes
    
    * Different variable name
    
    * Fix indenting
    
    * Change back unneeded stuff
    - callbackCalls didnt seem to do anything

--- examples/button-clicker/Assets/DiscordController.cs
+++ examples/button-clicker/Assets/DiscordController.cs
@@ -14,7 +14,6 @@ public class DiscordController : MonoBehaviour
     public DiscordRpc.RichPresence presence = new DiscordRpc.RichPresence();
     public string applicationId;
     public string optionalSteamId;
-    public int callbackCalls;
     public int clickCounter;
     public DiscordRpc.DiscordUser joinRequest;
     public UnityEngine.Events.UnityEvent onConnect;
@@ -52,41 +51,35 @@ public class DiscordController : MonoBehaviour
 
     public void ReadyCallback(ref DiscordRpc.DiscordUser connectedUser)
     {
-        ++callbackCalls;
         Debug.Log(string.Format("Discord: connected to {0}#{1}: {2}", connectedUser.username, connectedUser.discriminator, connectedUser.userId));
         onConnect.Invoke();
     }
 
     public void DisconnectedCallback(int errorCode, string message)
     {
-        ++callbackCalls;
         Debug.Log(string.Format("Discord: disconnect {0}: {1}", errorCode, message));
         onDisconnect.Invoke();
     }
 
     public void ErrorCallback(int errorCode, string message)
     {
-        ++callbackCalls;
         Debug.Log(string.Format("Discord: error {0}: {1}", errorCode, message));
     }
 
     public void JoinCallback(string secret)
     {
-        ++callbackCalls;
         Debug.Log(string.Format("Discord: join ({0})", secret));
         onJoin.Invoke(secret);
     }
 
     public void SpectateCallback(string secret)
     {
-        ++callbackCalls;
         Debug.Log(string.Format("Discord: spectate ({0})", secret));
         onSpectate.Invoke(secret);
     }
 
     public void RequestCallback(ref DiscordRpc.DiscordUser request)
     {
-        ++callbackCalls;
         Debug.Log(string.Format("Discord: join request {0}#{1}: {2}", request.username, request.discriminator, request.userId));
         joinRequest = request;
         onJoinRequest.Invoke(request);
@@ -104,10 +97,8 @@ public class DiscordController : MonoBehaviour
     void OnEnable()
     {
         Debug.Log("Discord: init");
-        callbackCalls = 0;
-
         handlers = new DiscordRpc.EventHandlers();
-        handlers.readyCallback = ReadyCallback;
+        handlers.readyCallback += ReadyCallback;
         handlers.disconnectedCallback += DisconnectedCallback;
         handlers.errorCallback += ErrorCallback;
         handlers.joinCallback += JoinCallback;
